d41915d7242c3a40958b151c300676d1cd8e6b07
Matt Penfield <matt.penfield@slalom.com>
2026-05-29 14:14:16 -0700

    Add LinkButton component for reusable link functionality in HomeView

---
+++ src/components/LinkButton.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+defineProps<{
+  label: string
+  url: string
+  icon: string
+}>()
+</script>
+
+<template>
+  <v-list-item
+    :href="url"
+    :prepend-icon="icon"
+    class="link-button mb-2"
+    border
+    rounded
+  >
+    <v-list-item-title class="text-uppercase font-weight-medium" style="letter-spacing: 0.08em;">
+      {{ label }}
+    </v-list-item-title>
+  </v-list-item>
+</template>
+
+<style scoped>
+.link-button {
+  transition: background 0.15s, color 0.15s;
+}
+
+.link-button:hover {
+  background: #000;
+  color: #fff;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import LinkButton from '@/components/LinkButton.vue'
+
 const links = [
   { label: 'Portfolio', url: '#', icon: 'mdi-briefcase-outline' },
   { label: 'Dribbble', url: '#', icon: 'mdi-basketball' },
@@ -20,19 +22,13 @@ const links = [
 
       <v-col cols="12" md="5">
         <v-list lines="one" bg-color="transparent" class="pa-0">
-          <v-list-item
+          <LinkButton
             v-for="link in links"
             :key="link.label"
-            :href="link.url"
-            :prepend-icon="link.icon"
-            class="link-item mb-2"
-            border
-            rounded
-          >
-            <v-list-item-title class="text-uppercase font-weight-medium" style="letter-spacing: 0.08em;">
-              {{ link.label }}
-            </v-list-item-title>
-          </v-list-item>
+            :label="link.label"
+            :url="link.url"
+            :icon="link.icon"
+          />
         </v-list>
       </v-col>
     </v-row>
@@ -47,15 +43,6 @@ const links = [
   letter-spacing: -0.04em;
   text-transform: uppercase;
   margin-top: 0;
-}
-
-.link-item {
-  transition: background 0.15s, color 0.15s;
-}
-
-.link-item:hover {
-  background: #000;
-  color: #fff;
 }
 
 @media (max-width: 680px) {
